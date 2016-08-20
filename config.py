d7d9559a58bf4ced00d4bb911df587ea24be15b1
John M. Wargo <john@johnwargo.com>
2016-08-20 17:51:27 -0400

    updated Sat 08/20/2016

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    # Weather Underground
+    STATION_ID = "KNCCHARL225"
+    STATION_KEY = "I4UL3GPT"
--- weather_station.py
+++ weather_station.py
@@ -12,9 +12,9 @@ import datetime
 import sys
 import time
 
-import Adafruit_DHT
-import ConfigParser
-import explorerhat
+from sense_hat import SenseHat
+
+from config import Config
 
 # import urllib2
 # from urllib import urlencode
@@ -36,25 +36,7 @@ slash_n = "\n"
 weather_underground_url = "http://weatherstation.wunderground.com/weatherstation/updateweatherstation.php"
 
 
-def config_section_map(section):
-    dict1 = {}
-    options = Config.options(section)
-    for option in options:
-        try:
-            dict1[option] = Config.get(section, option)
-            if dict1[option] == -1:
-                print("skip: %s" % option)
-        except:
-            print("exception on %s!" % option)
-            dict1[option] = None
-    return dict1
-
-
 def main():
-    # assign the sensor type
-    sensor = Adafruit_DHT.DHT22
-    # it's connected to pin 23
-    pin = 23
     # initialize the lastMinute variable to the current time to start
     last_minute = datetime.datetime.now().minute
     # on startup, just use the previous minute as lastMinute
@@ -76,54 +58,48 @@ def main():
             if (current_minute == 0) or ((current_minute % measurement_interval) == 0):
                 # get the reading timestamp
                 now = datetime.datetime.now()
-                # print("\nOn a", measurement_interval, "minute mark:", current_minute)
                 print("\n%d minute mark (%d @ %s)" % (measurement_interval, current_minute, str(now)))
-                # read the temperature
-                print("Turning the light on")
-                explorerhat.light.green.on()
-                print("Taking the measurement")
-                humidity, temp_c = Adafruit_DHT.read_retry(sensor, pin)
-                print("Turning the light off")
-                explorerhat.light.green.off()
-                if humidity is not None and temp_c is not None:
-                    # calculate temp in F and round to one decimal place
-                    temp_f = round((temp_c * 1.8) + 32, 1)
-                    # round the temp to one decimal place
-                    temp_c = round(temp_c, 1)
-                    # write the data to the console
-                    print('Temp: {0:0.1f}*F/{0:0.1f}*C  Humidity: {1:0.1f}%'.format(temp_f, temp_c, humidity))
 
-                    # ========================================================
-                    # Upload the weather data to Weather Underground
-                    # ========================================================
-                    # From http://wiki.wunderground.com/index.php/PWS_-_Upload_Protocol
-                    # print("\nUploading data to Weather Underground")
-                    # build a weather data object
-                    # weather_data = {
-                    #     "action": "updateraw",
-                    #     "ID": wu_station_id,
-                    #     "PASSWORD": wu_station_key,
-                    #     "dateutc": "now",
-                    #     "tempf": str(temp_f),
-                    #     "humidity": str(humidity)
-                    # }
-                    # try:
-                    #     upload_url = weather_underground_url + "?" + urlencode(weather_data)
-                    #     response = urllib2.urlopen(upload_url)
-                    #     html = response.read()
-                    #     print(html)
-                    #     # do something
-                    #     response.close()  # best practice to close the file
-                    # except:
-                    #     print("Exception:", sys.exc_info()[0], slash_n)
-                else:
-                    print('Failed to get reading.')
+                # ========================================================
+                # read the temperature from the Sense HAT
+                # ========================================================
+                humidity = round(sense.get_humidity(), 2)
+                # convert pressure from millibars to inHg before posting
+                pressure = round(sense.get_pressure() * 0.0295300, 2)
+                temp_c = round(sense.get_temperature(), 1)
+                temp_f = c_to_f(temp_c)
+                print("Temp: %sF (%sC), Pressure: %s inHg, Humidity: %s%%" % (temp_f, temp_c, pressure, humidity))
+
+                # ========================================================
+                # Upload the weather data to Weather Underground
+                # ========================================================
+                # From http://wiki.wunderground.com/index.php/PWS_-_Upload_Protocol
+                # print("\nUploading data to Weather Underground")
+                # build a weather data object
+                # weather_data = {
+                #     "action": "updateraw",
+                #     "ID": wu_station_id,
+                #     "PASSWORD": wu_station_key,
+                #     "dateutc": "now",
+                #     "tempf": str(temp_f),
+                #     "humidity": str(humidity)
+                # }
+                # try:
+                #     upload_url = weather_underground_url + "?" + urlencode(weather_data)
+                #     response = urllib2.urlopen(upload_url)
+                #     html = response.read()
+                #     print(html)
+                #     # do something
+                #     response.close()  # best practice to close the file
+                # except:
+                #     print("Exception:", sys.exc_info()[0], slash_n)
 
         # wait a second then check again
         # You can always increase the sleep value below to check less often
         time.sleep(1)  # this should never happen since the above is an infinite loop
 
     print("Leaving main()")
+
 
 # ============================================================================
 # here's where we start doing stuff
@@ -138,25 +114,12 @@ if (measurement_interval is None) or (measurement_interval > 60):
     print("The application's 'measurement_interval' cannot be empty or greater than 60")
     sys.exit(1)
 
-# calculate how many times we're checking temp in an hour.
-# used to manage last_values array length
-num_past_temp_values = int(60 / measurement_interval)
-
-# setup the config parser
-Config = ConfigParser.ConfigParser()
-print("\nOpening configuration file")
-try:
-    Config.read("./config.ini")
-except:
-    print("Exception:", sys.exc_info()[0], slash_n)
-    sys.exit(1)
-
 # ============================================================================
 #  Read Weather Underground Configuration Parameters
 # ============================================================================
 print("\nInitializing Weather Underground configuration")
-wu_station_id = config_section_map("WeatherUnderground")["station_id"]
-wu_station_key = config_section_map("WeatherUnderground")["station_key"]
+wu_station_id = Config.STATION_ID
+wu_station_key = Config.STATION_KEY
 if (wu_station_id is None) or (wu_station_key is None):
     print("Missing values from the Weather Underground configuration file\n")
     sys.exit(1)
@@ -165,6 +128,25 @@ print("URL:", weather_underground_url)
 print("Station ID:", wu_station_id)
 print("Station key:", wu_station_key)
 print("Successfully read Weather Underground configuration values")
+
+# ============================================================================
+# initialize the Sense HAT object
+# ============================================================================
+try:
+    print("\nInitializing the Sense HAT client")
+    sense = SenseHat()
+    # sense.set_rotation(180)
+    # then write some text to the Sense HAT's 'screen'
+    sense.show_message("Init", text_colour=[255, 255, 0], back_colour=[0, 0, 255])
+    # clear the screen
+    sense.clear()
+    # get the current temp to use when checking the previous measurement
+    last_temp = c_to_f(sense.get_temperature())
+    print("Current temperature:", last_temp)
+except:
+    print("Unable to initialize the Sense HAT library:", sys.exc_info()[0])
+    sys.exit(1)
+
 print("\nInitialization complete!\n")
 
 # Now see what we're supposed to do next

1c827610c865b2365984559cba391eceb44b8f8d
John M. Wargo <johnwargo@users.noreply.github.com>
2019-10-24 09:04:52 -0400

    updated Thu 10/24/2019

--- start-station.sh
+++ start-station.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# to configure the project to autostart, refer to the following article:
 # How To Autostart Apps In Rasbian LXDE Desktop
 # http://www.raspberrypi-spy.co.uk/2014/05/how-to-autostart-apps-in-rasbian-lxde-desktop/
 
